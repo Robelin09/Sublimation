54798941025f513d866db3df6165be49ea14eaab
Robelin Isaac Concepción Henriquez <robelin_concepcion@ucne.edu.do>
2024-03-31 12:45:42 -0400

    Creé mi Modelo VentasDetalle

---
+++ Shared/Models/VentasDetalle.cs
@@ -0,0 +1,20 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace Shared.Models;
+
+public class VentasDetalle
+{
+    [Key]
+    public int VentasDetalleId { get; set; }
+    [Required(ErrorMessage = "Debe asignar una fecha estimada")]
+    public DateTime FechaEstimadaEntrega { get; set; }
+
+    [Required(ErrorMessage = "Debe Ingresar la Dirección")]
+    public string? Direccion { get; set; }
+    public bool ServicioGrua { get; set; }
+    public bool Instalacion { get; set; }
+
+    [Required(ErrorMessage = "Este campo es requerido")]
+    [Range(1.00, float.MaxValue, ErrorMessage = "Debe ser mayor a 1.00")]
+    public float Costo { get; set; }
+}
