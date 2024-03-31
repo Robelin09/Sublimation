c3f140d9ade63cf7a0691aa28a75a55de089cf7c
PEDRO VLADIMIR GUZMAN SALAZAR <PEDRO_GUZMAN1@ucne.edu.do>
2024-03-31 13:08:11 -0400

    Agregue Modelo Compras

---
+++ Shared/Models/Compras.cs
@@ -0,0 +1,22 @@
+﻿using System.ComponentModel.DataAnnotations.Schema;
+using System.ComponentModel.DataAnnotations;
+
+namespace Shared.Models;
+
+public class Compras
+{
+    [Key]
+    public int ComprasId { get; set; }
+    [Required(ErrorMessage = "Debe seleccionar un Suplidor")]
+    [ForeignKey("SuplidorId")]
+    public int SuplidorId { get; set; }
+    [Required(ErrorMessage = "Debe seleccionar un usario")]
+    [ForeignKey("UsuarioId")]
+    public int UsuarioId { get; set; }
+    public DateTime FechaCompra { get; set; }
+    [Required(ErrorMessage = "Agregue una descripcion a esta compra")]
+    public string? Descripcion { get; set; }
+    [ForeignKey("ComprasId")]
+    public ICollection<ComprasDetalle> ComprasDetalle { get; set; } = new List<ComprasDetalle>();
+
+}
