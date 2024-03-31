781178eaf5d03e21fabc2ebfa4bac9858e989f29
PEDRO VLADIMIR GUZMAN SALAZAR <PEDRO_GUZMAN1@ucne.edu.do>
2024-03-31 13:09:51 -0400

    Agregue Contextos de los modelos Reclamos, Compras, Insumos y Suplidores

--- Sublimation/DAL/Contexto.cs
+++ Sublimation/DAL/Contexto.cs
@@ -1,15 +1,18 @@
 ﻿using Microsoft.EntityFrameworkCore;
 using Shared.Models;
 
-namespace Sublimation.DAL
-{
-    public class Contexto : DbContext
-    {
-        public DbSet<Clientes> Clientes { get; set; }
-        public DbSet<Usuarios> Usuarios { get; set; }
-        public DbSet<Ventas> Ventas { get; set; }
-        public DbSet<Servicios> Servicios { get; set; }
+namespace Sublimation.DAL;
 
-        public Contexto(DbContextOptions<Contexto> options) : base(options) { }
-    }
+public class Contexto : DbContext
+{
+    public DbSet<Clientes> Clientes { get; set; }
+    public DbSet<Usuarios> Usuarios { get; set; }
+    public DbSet<Ventas> Ventas { get; set; }
+    public DbSet<Servicios> Servicios { get; set; }
+    public DbSet<Reclamos> Reclamos { get; set; }
+    public DbSet<Compras> Compras { get; set; }
+    public DbSet<Insumos> Insumos { get; set; }
+    public DbSet<Suplidores> Suplidores { get; set; }
+
+    public Contexto(DbContextOptions<Contexto> options) : base(options) { }
 }
