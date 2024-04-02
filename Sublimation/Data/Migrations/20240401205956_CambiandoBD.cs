efef6a54f7b53f8d165e3a18c3238ae12e4d0928
Robelin Isaac Concepción Henriquez <robelin_concepcion@ucne.edu.do>
2024-04-02 12:40:57 -0400

    Cambios en las Paginas de "Compras"

---
+++ Sublimation/Data/Migrations/20240401205956_CambiandoBD.cs
@@ -0,0 +1,22 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace Sublimation.Migrations
+{
+    /// <inheritdoc />
+    public partial class CambiandoBD : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+
+        }
+    }
+}
