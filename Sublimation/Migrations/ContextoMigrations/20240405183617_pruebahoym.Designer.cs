7d2791922e47c1241f98f32929f69a900bfa862a
PEDRO VLADIMIR GUZMAN SALAZAR <PEDRO_GUZMAN1@ucne.edu.do>
2024-04-07 18:18:31 -0400

    Agredando migracion y datos

--- Sublimation/Migrations/ContextoMigrations/20240405183617_pruebahoym.Designer.cs
+++ Sublimation/Migrations/ContextoMigrations/20240405183617_pruebahoym.Designer.cs
@@ -2,6 +2,7 @@
 using System;
 using Microsoft.EntityFrameworkCore;
 using Microsoft.EntityFrameworkCore.Infrastructure;
+using Microsoft.EntityFrameworkCore.Migrations;
 using Microsoft.EntityFrameworkCore.Storage.ValueConversion;
 using Sublimation.DAL;
 
@@ -10,9 +11,11 @@ using Sublimation.DAL;
 namespace Sublimation.Migrations.ContextoMigrations
 {
     [DbContext(typeof(Contexto))]
-    partial class ContextoModelSnapshot : ModelSnapshot
+    [Migration("20240405183617_pruebahoym")]
+    partial class pruebahoym
     {
-        protected override void BuildModel(ModelBuilder modelBuilder)
+        /// <inheritdoc />
+        protected override void BuildTargetModel(ModelBuilder modelBuilder)
         {
 #pragma warning disable 612, 618
             modelBuilder.HasAnnotation("ProductVersion", "8.0.3");
