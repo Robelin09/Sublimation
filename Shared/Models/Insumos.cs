df41a74e346d0517664ad0a1214c3614146beb85
Robelin Isaac Concepción Henriquez <robelin_concepcion@ucne.edu.do>
2024-04-07 23:43:52 -0400

    Edité las Paginas de Insumos

--- Shared/Models/Insumos.cs
+++ Shared/Models/Insumos.cs
@@ -12,8 +12,7 @@ public class Insumos
     [Required(ErrorMessage = "Este campo es obligaotio")]
     [RegularExpression("^[a-zA-Z ]+$", ErrorMessage = "El Nombre debe contener solo letras.")]
     public string? NombreInsumo { get; set; }
-    [Required(ErrorMessage = "Este campo es obligaotio")]
-    [Range(1, int.MaxValue, ErrorMessage = "El Minimo que se pueden agregar son 5 unidades")]
+    
     public int Cantidad { get; set; }
     public DateTime fechaRegistro { get; set; }
     [Required(ErrorMessage = "Este campo es obligaotio")]
